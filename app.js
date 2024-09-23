ed10d771323de9ef95f3e901b2e29cf7c7f7cf6d
Vivek Vaghasiya <159127221+vivekvaghasiya133@users.noreply.github.com>
2024-09-23 10:33:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const http = require("http")
-const path = require('path');
+const http = require("http");
+const path = require("path");
 
-const socketio = require("socket.io")
-const server = http.createServer(app)
-const io = socketio(server)
-app.set('view engine' , 'ejs')
-app.use(express.static(path.join(__dirname, 'public')));
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
+const users = {}; // Store users' locations
 
-io.on("connection" , function(socket){
-    console.log("Connected");
+io.on("connection", function (socket) {
+  console.log("Connected: " + socket.id);
 
-    socket.on("send-location" , function(data){
-        io.emit("received-location" , {id : socket.id , ...data})
-    })
+  // Send all previous users' locations to the newly connected user
+  socket.emit("all-users", users);
 
-    socket.on("disconnect", function(){
-        io.emit("user-disconnected" ,socket.id)
-    })
-    
-})  
+  // Handle the user sending their location
+  socket.on("send-location", function (data) {
+    // Store the user's location
+    users[socket.id] = data;
 
-app.get('/' ,function(req , res){
-    res.render('index')
-})
+    // Broadcast the user's location to all other users
+    io.emit("received-location", { id: socket.id, ...data });
+  });
 
-server.listen(3000)
+  // Handle user disconnection
+  socket.on("disconnect", function () {
+    // Remove the user's data from the list
+    delete users[socket.id];
+
+    // Notify all clients about the disconnected user
+    io.emit("user-disconnected", socket.id);
+  });
+});
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+server.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
